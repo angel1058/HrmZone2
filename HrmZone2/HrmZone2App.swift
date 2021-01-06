7a8671d08ecd7765436dd6529262666fe47d3570
malcolm <malcolm@malcolmsmart.com>
2021-01-06 23:07:55 +0000

    Initial Commit

---
+++ HrmZone2/HrmZone2App.swift
@@ -0,0 +1,17 @@
+//
+//  HrmZone2App.swift
+//  HrmZone2
+//
+//  Created by malcolm on 06/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct HrmZone2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
